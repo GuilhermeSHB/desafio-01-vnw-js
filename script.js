e2a8ce4c9271fa194473a6966b5ce80c0bb78e53
GuilhermeSHB <guilhermeshb@gmail.com>
2022-01-25 18:31:31 -0300

    Push do desafio

---
+++ script.js
@@ -0,0 +1,35 @@
+//1 -  Crie uma variável que receba um nome;
+
+let nome = "Guilherme"
+
+//2 -  Crie uma variável que receba um número;
+
+let numero = 5
+
+//3 - Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let ano = 2022
+let sub = ano - numero
+
+console.log(sub)
+
+//4 - Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+let soma = ano + numero
+
+console.log(soma)
+
+//5 - Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log("Estou mexendo no console.log")
+
+//6 - Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = false
+
+console.log(quartaFeira)
+
+//7 - Exiba no console o tipo de dado da váriavel quartaFeira;
+
+let quartaFeira = "Dia da semana"
+console.log(typeof quartaFeira)
